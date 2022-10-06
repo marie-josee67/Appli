36de560df368e30821775cced4139b95b644539f
marie-josee67 <mariejosee89@hotmail.fr>
2022-10-06 12:01:47 +0200

    Base

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ajout produit</title>
+</head>
+<body>
+    <h1>Ajouter un produit</h1>
+    <form action="traitement.php" method="post">
+        <p>
+            <label>
+                Nom du produit : 
+                <input type="text" name="name">
+            </label>
+        </p>
+        <p>
+            <label>
+                Prix du produit :
+                <input type="number" step="any" name="price">
+            </label>
+        </p>
+        <p>
+            <label>
+                Quantité désirée :
+                <input type="number" name="qtt" value ="1">
+            </label>
+        </p>
+        <p>
+            <input type="submit" name="submit" value="Ajouter le produit">
+        </p>
+    </form>
+</body>
+</html>
