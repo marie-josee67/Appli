653d238308153ab2c779d853e5b728d7c51e4fe3
marie-josee67 <mariejosee89@hotmail.fr>
2022-10-06 15:50:15 +0200

    Correction de l'erreur de code entre la boucle  if  et le foreach

--- redap.php
+++ redap.php
@@ -31,15 +31,14 @@ produits présents en session. Elle doit également présenter le total de l'ens
         echo "<table>",
                 "<thead>",
                     "<tr>",
-                        "<td>".$index."</td>",
-                        "<td>".$product["name"]."</td>",
-                        "<td>".$product["price"]."</td>",
-                        "<td>".$product["qtt"]."</td>",
-                        "<td>".$product["total"]."</td>",
+                        "<th>#</th>",
+                        "<th>Nom</th>",
+                        "<th>Prix</th>",
+                        "<th>Quanité</th>",
+                        "<th>Total</th>",
                     "</tr>",
             "</table>",
             "<tbody>";
-
 /* La boucle itérative foreach(), particulièrement efficace pour exécuter, produit par produit, les mêmes instructions qui vont permettre l'affichage uniforme de chacun d'entre eux. Pour chaque donnée dans $_SESSION['products'], nous disposerons au sein de la boucle de deux variables :
 
 $index : aura pour valeur l'index du tableau $_SESSION['products'] parcouru. Nous pourrons numéroter ainsi chaque produit avec ce numéro dans le tableau HTML (en première colonne).
@@ -47,7 +46,14 @@ $index : aura pour valeur l'index du tableau $_SESSION['products'] parcouru. Nou
 $product : cette variable contiendra le produit, sous forme de tableau, tel que l'a créé et stocké en session le fichier traitement.php. */
 
         foreach($_SESSION["products"] as $index => $product) {
-
+            echo "<tr>",
+                "<td>".$index."</td>",
+                "<td>".$product["name"]."</td>",
+                /* La fonction PHP number_format() permet de modifier l'affichage d'une valeur numérique en précisant plusieurs paramètres. En ajoutant avant la fermeture de la balise <td> un symbole €, nos montants s'affichent en euro */
+                "<td>".number_format($product["price"], 2, ",", "&nbsp;")."&nbsp;€</td>",
+                "<td>".$product["qtt"]."</td>",
+                "<td>".number_format($product["total"], 2, ",", "&nbsp;")."&nbsp;€</td>",
+                "</tr>";
         }
         echo "</thody>",
         "</table>";
